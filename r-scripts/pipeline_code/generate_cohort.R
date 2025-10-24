dd270ec12fff8b797d1ae78fdc119450d45cd63b
Zach Welshman <38403427+zwelshman@users.noreply.github.com>
2025-10-24 12:27:14 +0000

    remove too much claude

--- r-scripts/pipeline_code/generate_cohort.R
+++
@@ -1,8 +0,0 @@
-# Generates a project specific demographics table.
-# patient, dob, sex, ethnicity, lsoa, 
-
-# reads from assets and applies criteria.
-# age 18 - 110 at index date
-# known sex
-# known ethnicity
-# lsoa at index date
